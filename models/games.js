6039ccfd152bfd5f146e107912a8f40eadee5bd0
Rob Weileder <weileder@gmatc.matc.edu>
2019-10-14 19:44:33 -0500

    initial commit

---
+++ models/games.js
@@ -0,0 +1,31 @@
+/*jshint esversion: 8 */
+import mongoose from 'mongoose';
+import { stringify } from 'querystring';
+
+const GameSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    platform: {
+        type: String,
+        required: true
+    },
+    developer: {
+        type: String,
+        required: true
+    },
+    genre: {
+        type: String,
+        required: true
+    },
+    esrb: {
+        type: String,
+        required: true
+    }
+});
+
+const game = mongoose.model('game', GameSchema);
+
+export default Game;
